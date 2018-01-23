47aeb61b639aacbfda5d02d21d0a38b6c876b5e8
Paul Davis <pjdavis@gmx.com>
2018-01-23 23:03:26 +0100

    update templates

--- charts/views.py
+++ charts/views.py
@@ -4,7 +4,25 @@ from .models import Site
 
 class SiteListView(generic.ListView):
     model = Site
-    queryset = Site.objects.order_by('region')
+    #queryset = Site.objects.order_by('region')
+
+    def get_context_data(self, **kwargs):
+        """
+        Get the context for this view.
+        """
+        regions1 = Site.objects.values_list('region')[:8]
+        regions2 = Site.objects.values_list('region')[8:]
+        queryset1 = Site.objects.filter(region__in=regions1).order_by('region')
+        queryset2 = Site.objects.filter(region__in=regions2).order_by('region')
+
+        context = {
+            'paginator': None,
+            'page_obj': None,
+            'is_paginated': False,
+            'object_list': queryset1,
+            'object_list3': queryset2
+        }
+        return context
 
 """
 class TweetMapView(generic.ListView):
