32db7bd83f533077516c8886f68c2e45fc9baf60
Paul Davis <pjdavis@gmx.com>
2018-01-28 00:49:13 +0100

    update tests

--- charts/tests.py
+++ charts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from charts.models import Site
 
 
 class ViewsTest(TestCase):
@@ -18,18 +19,31 @@ class ViewsTest(TestCase):
         self.assertTemplateUsed(resp, 'charts/site_list.html')
 
 
-    #assert that len of sites == 123
-    def test_pagination_is_ten(self):
-        resp = self.client.get(reverse('authors'))
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue('is_paginated' in resp.context)
-        self.assertTrue(resp.context['is_paginated'] == True)
-        self.assertTrue(len(resp.context['author_list']) == 10)
+class SiteModelTest(TestCase):
 
-    def test_lists_all_authors(self):
-        # Get second page and confirm it has (exactly) remaining 3 items
-        resp = self.client.get(reverse('authors') + '?page=2')
-        self.assertEqual(resp.status_code, 200)
-        self.assertTrue('is_paginated' in resp.context)
-        self.assertTrue(resp.context['is_paginated'] == True)
-        self.assertTrue(len(resp.context['author_list']) == 3)
+    @classmethod
+    def setUpTestData(cls):
+        #Set up non-modified objects used by all test methods
+        Site.objects.create(code='ABD', environment='Urban Background', latitude='57.157360', longitude='-2.094278',
+                            map_url='https://maps.google.co.uk/?q=57.157360,-2.094278', name='Aberdeen',
+                            region='North East Scotland', slug='ABD',
+                            url='https://uk-air.defra.gov.uk/networks/site-info?site_id=ABD')
+
+    def test_site_name_label(self):
+        site = Site.objects.get(id=1)
+        field_label = site._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'Site name')
+
+    def test_site_code_label(self):
+        site = Site.objects.get(id=1)
+        field_label = site._meta.get_field('code').verbose_name
+        self.assertEquals(field_label, 'Site code')
+
+    def test_map_url_max_length(self):
+        site = Site.objects.get(id=1)
+        max_length = site._meta.get_field('map_url').max_length
+        self.assertEquals(max_length,1000)
+
+    def test_get_absolute_url(self):
+        site=Site.objects.get(id=1)
+        self.assertEquals(site.get_absolute_url(), '/sites/ABD')
